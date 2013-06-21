7bff31d39087d8258e8737cf19a2b6893730ebe0
David Birtwell <dbirtwell@comcast.net>
2013-06-21 17:08:43 -0400

    1st pass commit
    This 1st pass is just a WIP. (Note ERB files for reference (these will
    be deleted)

---
+++ ratpack.groovy
@@ -0,0 +1,38 @@
+@GrabResolver("https://oss.jfrog.org/artifactory/repo")
+@Grab(value = "org.ratpack-framework:ratpack-groovy:0.9.0-SNAPSHOT")
+
+import org.ratpackframework.groovy.templating.TemplateRenderer
+import org.ratpackframework.groovy.templating.TemplatingModule
+import static org.ratpackframework.groovy.RatpackScript.ratpack
+
+ratpack {
+    modules {
+        get(TemplatingModule).setCacheSize(0)
+    }
+    
+    handlers {
+        // Only initialize this once
+        // def util = new TarotDeckUtil()
+
+        // default route
+        get {
+            get(TemplateRenderer).render "index.html", title: "Groovy Track My Stuff"
+        }
+		
+		get ("new") {
+            get(TemplateRenderer).render "new.html", title: "Groovy Track My Stuff"
+		}
+
+        //// Actual dynamic part
+        //get ("tarot") {
+        //    def card = util.randomCard()
+        //    get(TemplateRenderer).render "tarot.html", name: card.name, meaning: card.meaning, image: "images\\$card.image"
+        //}
+
+        get ("about") {
+            get(TemplateRenderer).render "about.html", title: "Groovy Track My Stuff"
+        }
+
+        assets "public"
+    }
+}
