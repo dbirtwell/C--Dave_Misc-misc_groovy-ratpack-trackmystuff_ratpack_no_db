d8fc156a72809b627c7b544f8139f79ec81327ab
David Birtwell <dbirtwell@comcast.net>
2013-06-24 12:15:30 -0400

    Revised form post and route

--- ratpack.groovy
+++ ratpack.groovy
@@ -5,9 +5,18 @@ import org.ratpackframework.groovy.templating.TemplateRenderer
 import org.ratpackframework.groovy.templating.TemplatingModule
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 
+import java.util.logging.Logger
+
 ratpack {
+
+    def logger = Logger.getLogger("")
+	
     modules {
         get(TemplatingModule).setCacheSize(0)
+		
+		// TODO:
+		// Enable non persistent map based session storage
+        // register(new MapSessionsModule(10, 5))
     }
     
     handlers {
@@ -29,7 +38,13 @@ ratpack {
 		// http://localhost:5050/edit/2
         get("delete/:id"){
           get(TemplateRenderer).render "delete.html", id: "${pathTokens.id}"
-        }				
+        }
+		
+		post ("submit") {
+            // get(TemplateRenderer).render "index.html", title: "Groovy Track My Stuff"
+			def form = request.form
+            response.send "Now saving: " + form.name + " of type: " + form.item_type
+        }								
 
         assets "public"
     }
